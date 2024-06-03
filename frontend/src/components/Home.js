516c899caa927f3bc14c055b4ef590532c81a54e
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-06-03 11:02:55 -0500

    Created Vite + React server

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,7 @@
-export function () {
-  return ();
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div><h1> Hi there! Welcome to the chat App</h1></div>
+  )
 }
-// rfc creates a function for you
