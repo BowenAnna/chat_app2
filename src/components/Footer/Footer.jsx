75e63233bee4d231d0cbfea78302c99f164ce0f3
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-06-07 14:32:54 -0400

    moved everything from subdirectory chat-app to the main chat-app directory

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <div className={styles.display}>
+        <p>Contact me</p> 
+      <div>
+        <p>Linked In</p>
+        <p>Email</p>
+      </div>
+        </div>
+  )
+}
